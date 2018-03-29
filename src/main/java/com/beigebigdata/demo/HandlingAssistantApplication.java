2dbf9b7ee1f6f31046f9346bafb31006c47d73c3
lin <lin.maple.leaf@gmail.com>
2018-03-29 10:22:13 +0800

    初始上传

---
+++ src/main/java/com/beigebigdata/demo/HandlingAssistantApplication.java
@@ -0,0 +1,12 @@
+package com.beigebigdata.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandlingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandlingAssistantApplication.class, args);
+	}
+}
